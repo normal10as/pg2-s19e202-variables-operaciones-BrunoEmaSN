688766162ee2c0d38b1a070223f427cf0ec3289d
the penumbre <the_penumbre@hotmail.com>
2019-05-06 21:55:24 -0300

    TP listo

--- Variables/cuadratica/equivalencia_dias.vb
+++ Variables/cuadratica/equivalencia_dias.vb
@@ -3,16 +3,13 @@ Imports System
 Module equivalencia_dias
     Sub Main()
         Dim numero As Integer
-        Dim ds As Int16
-        Dim hd As Int16
-        Dim mh As Int16
+        Const ds As Int16 = 7
+        Const hd As Int16 = 24
+        Const mh As Int16 = 60
 
         Console.WriteLine("Ingresar un numero")
         numero = Console.ReadLine()
-        ds = numero / 7
-        hd = numero * 24
-        mh = hd * 60
-        Console.WriteLine("Numero de Semanas: " & ds & ", Numero de Horas: " & hd & " y Numero de Minutos: " & mh)
+        Console.WriteLine("Numero de Semanas: " & numero / ds & ", Numero de Horas: " & numero * hd & " y Numero de Minutos: " & (numero * hd) * mh)
         Console.Read()
     End Sub
 End Module
