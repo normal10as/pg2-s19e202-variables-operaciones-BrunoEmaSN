77fa54e543a341ff6e5be6fd4bf1bce3d9779f45
the penumbre <the_penumbre@hotmail.com>
2019-05-06 21:33:03 -0300

    TP Terminado

--- Variables/Reafrase/Refrase.vb
+++ Variables/Reafrase/Refrase.vb
@@ -4,9 +4,10 @@
         Dim Text As String = "Aunque la cadena sea de oro, sirve para lo mismo"
         Dim TestString As String
         Dim TestString2 As String
-        Console.WriteLine("Ingrese una Palabra")
+        Console.WriteLine("Ingrese una Palabra a remplazar en esta frase ")
+        Console.WriteLine("(" & Text & ")")
         TestString = Console.ReadLine()
-        Console.WriteLine("Ingrese otra Palabra")
+        Console.WriteLine("Ingrese Palabra para remplazar")
         TestString2 = Console.ReadLine()
         Console.WriteLine("Reemplazar en '{0}' la {1} por la {2}", Text, TestString, TestString2)
         Console.WriteLine(Replace(Text, TestString, TestString2))
